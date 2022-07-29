fc7b9a2969560838de14f6573c4885afd48b5a41
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-07-28 17:34:23 -0700

    change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: 'http://192.168.1.116:3000/'
+        origin: 'http://localhost:3000'
     }
 ));
 
